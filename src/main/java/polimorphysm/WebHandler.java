c205b05c19d1a795035e356aa70b505c7a4806be
Evgeny Borisov <bsevgeny@gmail.com>
2016-06-13 11:41:17 +0300

    Initial commit

---
+++ src/main/java/polimorphysm/WebHandler.java
@@ -0,0 +1,10 @@
+package polimorphysm;
+
+/**
+ * Created by Jeka on 13/06/2016.
+ */
+public interface WebHandler {
+    void handle();
+
+    int getMyCode();
+}
---
+++ src/main/java/polimorphysm/WebHandlerImpl.java
@@ -0,0 +1,14 @@
+package polimorphysm;
+
+/**
+ * Created by Jeka on 13/06/2016.
+ */
+public class WebHandlerImpl implements WebHandler {
+    public void handle() {
+
+    }
+
+    public int getMyCode() {
+        return 0;
+    }
+}
